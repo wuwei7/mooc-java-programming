edd767aa3fe1a58a3bc86d5ffdbec2bfbf43ee63
Brandon Campos <16386564+wuwei7@users.noreply.github.com>
2023-12-01 11:57:33 -0300

    Add exercises for part 2.3

--- mooc-java-programming-i/part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ mooc-java-programming-i/part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -6,6 +6,16 @@ public class FromWhereToWhere {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.print("Where to? ");
+        int toNumber = Integer.valueOf(scanner.nextLine());
+        System.out.print("Where from? ");
+        int fromNumber = Integer.valueOf(scanner.nextLine());
+        
+        int i = fromNumber;
+        while (i <= toNumber) {
+            System.out.println(i);
+            i++;
+        }
+        
     }
 }
