080b39a4c62ef0cc31cdbc791988e5458ffacc43
Brandon Campos <16386564+wuwei7@users.noreply.github.com>
2023-12-12 15:33:12 -0300

    Add exercises for part 2.4

--- mooc-java-programming-i/part02-Part02_29.Summation/src/main/java/Summation.java
+++ mooc-java-programming-i/part02-Part02_29.Summation/src/main/java/Summation.java
@@ -1,8 +1,8 @@
 public class Summation {
 
     public static int sum(int num1, int num2, int num3, int num4) {
-        // write some code here
-        return -1;
+        int sum = num1 + num2 + num3 + num4;
+        return sum;
     }
 
     public static void main(String[] args) {
