282830749a3f1deaf1198dc9913140a65b43c6a2
Brandon Campos <16386564+wuwei7@users.noreply.github.com>
2023-12-13 16:46:29 -0300

    Add exercises for part 2.4

--- mooc-java-programming-i/part02-Part02_33.StarSign/src/main/java/StarSign.java
+++ mooc-java-programming-i/part02-Part02_33.StarSign/src/main/java/StarSign.java
@@ -2,11 +2,6 @@
 public class StarSign {
 
     public static void main(String[] args) {
-
-        
-        //The tests are not checking the main, so you can modify it freely.
-        //NB: If the tests don't seem to pass, you should try the methods here
-        //in the main to make sure they print the correct shapes!
         
         printStars(3);
         System.out.println("\n---");  // printing --- between the shapes
@@ -19,18 +14,36 @@ public class StarSign {
     }
 
     public static void printStars(int number) {
-        // first part of the exercise
+        int i = 0;
+        while (i < number) {
+            System.out.print("*");
+            i++;
+        }
+        
+        System.out.println("");
     }
 
     public static void printSquare(int size) {
-        // second part of the exercise
+        int i = 0;
+        while (i < size) {
+            printStars(size);
+            i++;
+        }
     }
 
     public static void printRectangle(int width, int height) {
-        // third part of the exercise
+        int i = 0;
+        while (i < height) {
+            printStars(width);
+            i++;
+        }
     }
 
     public static void printTriangle(int size) {
-        // fourth part of the exercise
+        int i = 1;
+        while (i <= size) {
+            printStars(i);
+            i++;
+        }
     }
 }
